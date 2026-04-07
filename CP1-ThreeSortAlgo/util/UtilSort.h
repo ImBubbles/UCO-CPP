6f7cabb89c302e65e94c154739c174d4d35d5a9a
ImBubbles <bubblybubblesbubblesup@gmail.com>
2026-04-07 13:18:06 -0500

    Optimizations

--- CP1-ThreeSortAlgo/util/UtilSort.h
+++ CP1-ThreeSortAlgo/util/UtilSort.h
@@ -2,24 +2,24 @@
 #include <functional>
 
 namespace UtilSort {
-    template <typename T>
-    void insertionSort(T* arr, const int size, const std::function<bool(const T& key, const T& prev)>& conditonal) {
+    template <typename T, typename Comparator>
+    void insertionSort(T* arr, const int size, Comparator comp) {
         for(int i = 1; i < size; i++) {
         T key = arr[i];
         int j = i - 1;
-        while(j >=0 && conditonal(key, arr[j])) { // should prev be shifted forward one
+        while(j >=0 && comp(key, arr[j])) { // should prev be shifted forward one
             arr[j + 1] = arr[j];
             j--;
         }
         arr[j + 1] = key;
         }
     }
-    template <typename T>
-    void selectionSort(T* arr, const int size, const std::function<bool(const T& key, const T& against)> conditional) {
+    template <typename T, typename Comparator>
+    void selectionSort(T* arr, const int size, Comparator comp) {
         for(int i = 0; i < size -1; i++) {
             int swapPos = i;
             for(int j = i + 1; j < size; j++) {
-                if(conditional(arr[j], arr[swapPos])) {
+                if(comp(arr[j], arr[swapPos])) {
                     swapPos = j; // Should key swap with against? Against will be the lowest condition by the end of loop
                 }
             }
@@ -30,11 +30,11 @@ namespace UtilSort {
             }
         }
     }
-    template <typename T>
-    void bubbleSort(T* arr, const int size, const std::function<bool(const T& current, const T& next)> conditional) {
+    template <typename T, typename Comparator>
+    void bubbleSort(T* arr, const int size, Comparator comp) {
         for(int i = 0; i < size - 1; i++) {
             for(int j = 0; j < size - i -1; j++) {
-                if(conditional(arr[j], arr[j + 1])) { // Should swap current with next?
+                if(comp(arr[j], arr[j + 1])) { // Should swap current with next?
                     T temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -53,39 +53,27 @@ namespace UtilSort {
 
         template <typename T>
         void ltgInsertionSort(T* arr, const int size) {
-            return UtilSort::insertionSort(arr, size, std::function<bool(const T&, const T&)>([](const T& key, const T& prev) {
-                return key < prev;
-            }));
+            insertionSort(arr, size, [](const T& key, const T& prev) { return key < prev; });
         }
         template <typename T>
         void gtlInsertionSort(T* arr, const int size) {
-            return UtilSort::insertionSort(arr, size, std::function<bool(const T&, const T&)>([](const T& key, const T& prev) {
-                return key > prev;
-            }));
+            insertionSort(arr, size, [](const T& key, const T& prev) { return key > prev; });
         }
         template <typename T>
         void ltgSelectionSort(T* arr, const int size) {
-            return UtilSort::selectionSort(arr, size, std::function<bool(const T&, const T&)>([](const T& key, const T& against) {
-                return against > key;
-            }));
+            selectionSort(arr, size, [](const T& key, const T& against) { return key < against; });
         }
         template <typename T>
         void gtlSelectionSort(T* arr, const int size) {
-            return UtilSort::selectionSort(arr, size, std::function<bool(const T&, const T&)>([](const T& key, const T& against) {
-                return against < key;
-            }));
+            selectionSort(arr, size, [](const T& key, const T& against) { return key > against; });
         }
         template <typename T>
         void ltgBubbleSort(T* arr, const int size) {
-            return UtilSort::bubbleSort(arr, size, std::function<bool(const T&, const T&)>([](const T& current, const T& next) {
-                return current > next;
-            }));
+            bubbleSort(arr, size, [](const T& current, const T& next) { return current > next; });
         }
         template <typename T>
         void gtlBubbleSort(T* arr, const int size) {
-            return UtilSort::bubbleSort(arr, size, std::function<bool(const T&, const T&)>([](const T& current, const T& next) {
-                return current < next;
-            }));
+            bubbleSort(arr, size, [](const T& current, const T& next) { return current < next; });
         }
     }
 };
